e768329845f177c8da4813eb728a5c423b0d7a87
aros131 <129106832+aros131@users.noreply.github.com>
2024-12-08 19:00:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ app.use(cors());
 app.use(bodyParser.json()); // Parse incoming request bodies as JSON
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log('MongoDB connection error:', err));
+mongoose.connect('mongodb+srv://arenkaraseki:azxazx23@productreviewcluster.hfgko.mongodb.net/?retryWrites=true&w=majority', 
+    { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log('MongoDB connection error:', err));
 
 // Product Schema
 const productSchema = new mongoose.Schema({
