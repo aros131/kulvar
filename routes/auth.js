b90b3d96353ca1630af024d98377201b2f8c364d
aros131 <129106832+aros131@users.noreply.github.com>
2025-01-11 22:36:20 +0300

    Added .gitignore to exclude node_modules

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { User } = require("../models"); // Adjust the path to match your models folder
 const router = express.Router();
