a804a7a09d2aea610e2a9fd2e31f2b1143e1762d
aros131 <129106832+aros131@users.noreply.github.com>
2025-06-27 22:21:33 +0300

    h

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,14 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://aros131.github.io',
+  origin: [
+    'https://aros131.github.io',
+    'https://kulvar-iwqi-2kslsx509-aros131s-projects.vercel.app',
+    'http://localhost:3000'
+  ],
   credentials: true
 }));
+
 app.use(express.json());
 
 // MongoDB Connection
