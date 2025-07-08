7c5df154fdd3213b15bf20a36e2f8a4076578df4
aros131 <129106832+aros131@users.noreply.github.com>
2025-07-08 12:36:26 +0300

    h

--- frontend/src/components/program/ProgressChart.tsx
+++ frontend/src/components/program/ProgressChart.tsx
@@ -30,7 +30,7 @@ const ProgressChart: React.FC<ProgressChartProps> = ({ completionPercentage }) =
   return (
     <div className="bg-white dark:bg-gray-900 rounded-2xl shadow p-6 mb-4">
       <h2 className="text-lg font-semibold mb-2">Program İlerleme</h2>
-      <Doughnut data={data} width={200} height={200} />
+      <Doughnut data={data} width={100} height={100} />
 
     </div>
   );
