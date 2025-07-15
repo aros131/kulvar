d4c0b787ea7c67a3b33ab1b80b0e14ee5b9cfd42
aros131 <129106832+aros131@users.noreply.github.com>
2025-07-15 13:38:06 +0300

    k

--- frontend/src/app/dashboard/coach/programs/[programId]/page.tsx
+++ frontend/src/app/dashboard/coach/programs/[programId]/page.tsx
@@ -1,7 +1,11 @@
 import ProgramGeneralForm from "@/components/program/edit/ProgramGeneralForm";
 
-export default async function ProgramEditPage({ params }: { params: { programId: string } }) {
-  const { programId } = params;
+export default async function ProgramEditPage({ 
+  params 
+}: { 
+  params: Promise<{ programId: string }> 
+}) {
+  const { programId } = await params;
 
   const res = await fetch(`https://kulvar-qb7t.onrender.com/programs/${programId}`, {
     headers: {
