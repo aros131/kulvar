b8a45b9977a003dd6f279f40928b43cf655f10a8
aros131 <129106832+aros131@users.noreply.github.com>
2025-08-06 20:37:48 +0300

    k

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{ts,tsx,js,jsx}",
-    "./components/**/*.{ts,tsx,js,jsx}",
+   
     "./src/**/*.{ts,tsx,js,jsx}",
-    "./app/globals.css"
+    "./src/app/globals.css",
   ],
   theme: {
     extend: {
