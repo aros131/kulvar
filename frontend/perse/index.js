3a6a0ab72b497e86db56ba05e76a884172981b4c
aros131 <129106832+aros131@users.noreply.github.com>
2025-08-10 21:29:47 +0300

    fix lint errors in firebase functions example

--- frontend/perse/index.js
+++ frontend/perse/index.js
@@ -7,9 +7,10 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+import { setGlobalOptions } from "firebase-functions";
+// Uncomment the lines below to use HTTPS functions and logging
+// import { onRequest } from "firebase-functions/https";
+// import logger from "firebase-functions/logger";
 
 // For cost control, you can set the maximum number of containers that can be
 // running at the same time. This helps mitigate the impact of unexpected
